f5fd2dd269b9e776ae1e2de98cd63e6e0e8e6f06
Tim Hunter <thunte27@uwo.ca>
2022-04-08 23:48:50 -0400

    Add dynamic recipe page

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,40 +1,43 @@
+import Link from "next/link";
 import React, { FC } from "react";
 import { Panel, Rate, Stack } from "rsuite";
 import { Recipe } from "../lib/models";
 
 const RecipeCard: FC<{ recipe: Recipe; style?: any }> = ({ recipe, style }) => {
   return (
-    <Panel bordered defaultExpanded={false} style={style}>
-      <img
-        src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F02%2F17%2F21014-Good-old-Fashioned-Pancakes-mfs_002.jpg"
-        height="240"
-      />
-      <Panel header={recipe.name}>
-        {/* Rating */}
-        <Stack>
-          <Rate
-            readOnly
-            defaultValue={
-              recipe.ratings
-                ? recipe.ratings.reduce(
-                    (total, rating) => total + rating.value,
-                    0
-                  ) / recipe.ratings.length
-                : 0
-            }
-            allowHalf
-          />
+    <Link href={`/recipe/${recipe._id}`}>
+      <Panel bordered defaultExpanded={false} style={style} shaded>
+        <img
+          src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F02%2F17%2F21014-Good-old-Fashioned-Pancakes-mfs_002.jpg"
+          height="240"
+        />
+        <Panel header={recipe.name}>
+          {/* Rating */}
+          <Stack>
+            <Rate
+              readOnly
+              defaultValue={
+                recipe.ratings
+                  ? recipe.ratings.reduce(
+                      (total, rating) => total + rating.value,
+                      0
+                    ) / recipe.ratings.length
+                  : 0
+              }
+              allowHalf
+            />
+            <p>
+              <small>{recipe.ratings.length}</small>
+            </p>
+          </Stack>
+          {/* Description */}
+          {/* Author */}
           <p>
-            <small>{recipe.ratings.length}</small>
+            <small>{recipe.description}</small>
           </p>
-        </Stack>
-        {/* Description */}
-        {/* Author */}
-        <p>
-          <small>{recipe.description}</small>
-        </p>
+        </Panel>
       </Panel>
-    </Panel>
+    </Link>
   );
 };
 
---
+++ src/pages/recipe/[id].tsx
@@ -0,0 +1,61 @@
+import { useRouter } from "next/router";
+import React from "react";
+import { useSelector } from "react-redux";
+import { FlexboxGrid, Panel, Rate, Stack } from "rsuite";
+import { selectRecipes } from "../../features/user/recipeSlice";
+import { Recipe } from "../../lib/models";
+import Image from "next/image";
+
+const Recipe = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  let recipes: Recipe[] = useSelector(selectRecipes);
+
+  let recipe = recipes.find((value) => value._id.toString() == id.toString());
+
+  if (!recipe) {
+    return <p>Loading</p>;
+  }
+
+  return (
+    <Panel bordered defaultExpanded={false} shaded>
+      <FlexboxGrid justify="space-around">
+        <FlexboxGrid.Item
+          colspan={18}
+          as={"img"}
+          src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F02%2F17%2F21014-Good-old-Fashioned-Pancakes-mfs_002.jpg"
+          style={{ width: 360 }}
+          //   width={240}
+        ></FlexboxGrid.Item>
+      </FlexboxGrid>
+      <Panel header={recipe.name}>
+        {/* Rating */}
+        <Stack>
+          <Rate
+            readOnly
+            defaultValue={
+              recipe.ratings
+                ? recipe.ratings.reduce(
+                    (total, rating) => total + rating.value,
+                    0
+                  ) / recipe.ratings.length
+                : 0
+            }
+            allowHalf
+          />
+          <p>
+            <small>{recipe.ratings.length}</small>
+          </p>
+        </Stack>
+        {/* Description */}
+        {/* Author */}
+        <p>
+          <small>{recipe.description}</small>
+        </p>
+      </Panel>
+    </Panel>
+  );
+};
+
+export default Recipe;
